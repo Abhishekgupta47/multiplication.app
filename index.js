b7de2ab231442e4059eec863db36e0f7b43a5a05
abhishek gupta <abhishekgupta0301.com>
2022-11-28 13:37:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const num1 =Math.ceil(Math.random()*10);
+const num2 =Math.ceil(Math.random()*10);
+
+const questionE1=document.getElementById("question");
+questionE1.innerText=`what is ${num1}
+multiply by ${num2}?`
